36226dfb4fcd75b6a98bc83aea7806a699b37be5
analmar <analmar@upv.es>
2021-03-03 22:46:21 +0100

    primer py

--- expReg.py
+++ expReg.py
@@ -2,33 +2,92 @@ import os
 import re
 from pathlib import Path
 
-# txt = Path(os.getcwd()+"/datosOr/test2.txt").read_text()
-# f = open (os.getcwd()+"/datosOr/test2.txt","r")
+from tkinter import filedialog
+from tkinter import *
 
-c = '<!----><li ng-repeat="wo in filtered | orderBy:orderBy:reverse | limitTo:limit" class="diary-list__workout activity-id-2"><a href="https://www.sports-tracker.com/workout/analmar/603b4eccb357342885b873de" target="_blank"><span class="activity-icon" activity-icon="2"><svg><use xlink:href="#activity-icon-2"></use></svg></span> <span class="description" title="">&nbsp;</span> <span class="date">Feb 28, 2021</span> <span class="duration">01:48:41</span> <span class="distance">31.92 km</span> <span class="avg-speed">17.6 km/h</span> <span class="avg-pace">3:24/km</span> <span class="hr">0 bpm&nbsp;</span> <span class="energy">1095 kcal</span> <span class="cadence">0 rpm</span></a></li><!----><li ng-repeat="wo in filtered | orderBy:orderBy:reverse | limitTo:limit" class="diary-list__workout activity-id-0"><a href="https://www.sports-tracker.com/workout/analmar/603a9b911e91b31dd5492533" target="_blank"><span class="activity-icon" activity-icon="0"><svg><use xlink:href="#activity-icon-0"></use></svg></span> <span class="description" title="">&nbsp;</span> <span class="date">Feb 27, 2021</span> <span class="duration">01:32:19</span> <span class="distance">6.75 km</span> <span class="avg-speed">4.4 km/h</span> <span class="avg-pace">13:39/km</span> <span class="hr">0 bpm&nbsp;</span> <span class="energy">421 kcal</span> <span class="cadence">0 rpm</span></a></li><!----><li ng-repeat="wo in filtered | orderBy:orderBy:reverse | limitTo:limit" class="diary-list__workout activity-id-2"><a href="https://www.sports-tracker.com/workout/analmar/6039f1c0b12865370fadf82b" target="_blank"><span class="activity-icon" activity-icon="2"><svg><use xlink:href="#activity-icon-2"></use></svg></span> <span class="description" title="">&nbsp;</span> <span class="date">Feb 27, 2021</span> <span class="duration">01:36:45</span> <span class="distance">30.07 km</span> <span class="avg-speed">18.6 km/h</span> <span class="avg-pace">3:13/km</span> <span class="hr">0 bpm&nbsp;</span> <span class="energy">1007 kcal</span> <span class="cadence">0 rpm</span></a></li><!----><li ng-repeat="wo in filtered | orderBy:orderBy:reverse | limitTo:limit" class="diary-list__workout activity-id-2"><a href="https://www.sports-tracker.com/workout/analmar/6037d76250cc5169d5b91b64" target="_blank"><span class="activity-icon" activity-icon="2"><svg><use xlink:href="#activity-icon-2"></use></svg></span> <span class="description" title="">&nbsp;</span> <span class="date">Feb 25, 2021</span> <span class="duration">00:58:08</span> <span class="distance">20.10 km</span> <span class="avg-speed">20.7 km/h</span> <span class="avg-pace">2:53/km</span> <span class="hr">135 bpm&nbsp;</span> <span class="energy">779 kcal</span> <span class="cadence">0 rpm</span></a></li>'
+ventana= Tk()
+def abrir_archivo():
+    archivo_abierto=filedialog.askopenfilename(initialdir="/mnt",
+        title="Selecciona Archivo",
+        filetypes=( ("html","*.htm?"),("todas","*.*") ) )
+    return archivo_abierto
+    #print(archivo_abierto)
+
+#Button(text="Seleccionar Archivo",bg="pale green",command=abrir_archivo).place(x=10,y=10)
+
+fich=''
+fich=abrir_archivo()
+
+print (fich)
+if not isinstance(fich,str) :
+    exit(66)
+#######################################################
+
+estilo="\n<style>" \
+  "\ntable {" \
+  "\n  font-family: arial, sans-serif;" \
+  "\n  border-collapse: collapse;" \
+  "\n  width: 100%;}" \
+"\ntd, th {" \
+"\n  border: 1px solid #dddddd;" \
+"\n  text-align: left;"\
+"\n  padding: 8px;}"\
+"\ntr:nth-child(even) {"\
+"\n  background-color: #dddddd;}"\
+"\n</style>"
+
+Origen = Path(fich).read_text()
+
+Destino =  open(fich + "_new_.html","w")
+# c = open (os.getcwd()+"/datosOr/test2.txt","r")
+
 p = "<li.*?(activity-id-[0-9]{1,3}).*?>.*?(<a href.*?>)(.*?)<\/a><\/li>"
-m = re.findall(p, c)
+m = re.findall(p, Origen)
 
 print("...INI...")
-p2 = ""
+
+Destino.write("<html>")
+Destino.write("\n<head>")
+Destino.write(estilo)
+Destino.write("\n</head>")
+Destino.write("\n<Body>\n<table>")
+Destino.write("\n<tr>")
+Destino.write("\n\t<th>&nbsp;</th>")
+Destino.write('\n\t<th class="activity-head">Actividad</th>')
+Destino.write('\n\t<th class="description-head" >Descripcion</th>')
+Destino.write('\n\t<th class="date-Head">Fecha</th>')
+Destino.write('\n\t<th class="duration-head">duracion</th>')
+Destino.write('\n\t<th claSs="distance-head">Distancia</th>')
+Destino.write('\n\t<th class="avg-speed-head">Vel.Media</th>')
+Destino.write('\n\t<th class="avg-pace-head">paso km</th>')
+Destino.write('\n\t<th class="hr-head">pulsaciones</th>')
+Destino.write('\n\t<th class="energy-head">Calorias</th>')
+Destino.write('\n\t<th class="cadence-head">cadencia</th>')
+Destino.write('\n\t</tr>')
+
 
 for i in range(0, len(m)):
     g = m[i]
-    p1 = "\n<tr " + g[0] + ">\n\t" + "<td>" + g[1] + g[0] + "</a></td>" + "\n\t" + g[2] + "\n</tr>\n"
-    p = "(.*?)<span(.*?)>(.* ?)<\/span>(.*?)"
-    #print(p1)
-    m2 = re.findall(p, p1)
-    #print(m2)
-    for j in range(0, len(m2)):
-        g2 = m2[j]
-        p2 = p2 + g2[0] + "\n\t<td" + g2[1] + ">" + g2[2] + "</td>" + g2[3]
-        print(p2)
+    #p1 = "\n<tr " + g[0] + ">\n\t" + "<td>" + g[1] + g[0] + "</a></td>" + "\n\t" + g[2] + "\n</tr>\n"
+    p1 = "\n<tr>\n\t" + "<td>" + g[1] +  "ir</a></td>" + "\n\t" + g[2] + "\n</tr>"
+    p1 = p1.replace("<span","\n\t<td")
+    p1 = p1.replace("</span>", "</td>")
+    p1 = re.sub('<svg.*\/svg>','',p1)
+    p1 = p1.replace("</span>", "</td>")
+    p = ' activity-icon="2">'
+    p1 = re.sub(p,'>Correr',p1)
+    p = ' activity-icon="0">'
+    p1 = re.sub(p,'>Andar',p1)
+    p = ' activity-icon="69">'
+    p1 = re.sub(p,'>Aerobico',p1)
+    p = ' activity-icon="23">'
+    p1 = re.sub(p,'>Pesas',p1)
+    p = ' activity-icon="32">'
+    p1 = re.sub(p,'>Fitness',p1)
+    Destino.write(p1)
 
-    ## p2: str = re.sub(r'<span(. *?)>(.* ?)</span>',"\n\t<td$1>$2</td>",p1)
-
-
-
-    # \n < tr $1 >\n\t < td >$2$1 < / a > < / td >$3\n < / tr >\n
-    # print(m[i])
+Destino.write("\n</table>\n</Body>")
+Destino.write("\n</html>")
 
 print("...FIN...")
+Destino.close()
